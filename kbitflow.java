d466eb31574b97bb7dda1882515cd84cbfb313ed
MD SHAKIL <159142294+mdShakil2004@users.noreply.github.com>
2026-03-12 23:25:32 +0530

    day 56

---
+++ kbitflow.java
@@ -0,0 +1,29 @@
+// kbit
+
+
+  class Solution {
+    public int kBitFlips(int[] arr, int k) {
+        int n = arr.length;
+        int[] isFlipped = new int[n];
+        int flip = 0;
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            if (i >= k) {
+                flip ^= isFlipped[i - k];
+            }
+
+            if (arr[i] == flip) {
+
+                if (i + k > n) return -1;
+
+                ans++;
+                flip ^= 1;
+                isFlipped[i] = 1;
+            }
+        }
+
+        return ans;
+    }
+}
