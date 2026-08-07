19d65c92a0675800daca814a8f48725686aeebf0
MD SHAKIL <159142294+mdShakil2004@users.noreply.github.com>
2026-08-07 23:31:21 +0530

    Create 169.java

---
+++ 169.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int countFriendsPairings(int n) {
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+
+        long[] dp = new long[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + (i - 1) * dp[i - 2];
+        }
+
+        return (int) dp[n];
+    }
+}
